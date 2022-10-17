a2e65701fc13edc9264e26aa8ba68afcd04f2d0d
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2022-10-16 21:23:03 -0300

    catalogo super sayayin rojo

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
-let url = "https://my-json-server.typicode.com/IsmaelAxel/API_Pelis/peliculas";
-let datos = (apiUrl) => {
-  return fetch(apiUrl)
-    .then(response => response.json())
-    .then( json => { pelis(json);})
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        url: "https://my-json-server.typicode.com/IsmaelAxel/API_Pelis/peliculas",
+        pelis:[]
+      }
+    },
+    methods:{
+        fetchData(url){
+            fetch(url)
+            .then(response => response.json())
+            .then(data => this.pelis=data
+            )
+        }
+    },
+    created(){
+        this.fetchData(this.url)
     }
 
+  }).mount('#app')
 
-let pelis = (_data) =>{
-    let html = '';
-    _data.forEach(m => {
-        html += `<div>
-        <div class="tarjeta fondoFooter">
-        <img src= "${m.Poster}" alt="${m.Title}">
-        <div class= "cuerpo">
-        <h4>${m.Title}</h4>
-        <br>
-        <h5>Director: ${m.Director}</h5>
-        <h5>Lanzamiento: ${m.Released}</h5>
-        <h5>Duracion: ${m.Runtime}</h5>
-        <h5>Tipo: ${m.Type}</h5>
-        <br>
-        <a href="${m.Trailer}" target="_blank"><button class="btn">Más información</button></a> 
-    </div>
-    </div>
-    </div>`;
-    });
-    document.getElementById('card').innerHTML = html
-}
-
-datos(url);
