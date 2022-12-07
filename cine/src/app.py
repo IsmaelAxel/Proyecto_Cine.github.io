a8bc3b80e1ac01a79e6df0f134e9c614ba148e56
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2022-12-07 17:21:45 -0300

    modifico app.py y agrego templatesa virtualenv cine

--- cine/src/app.py
+++ cine/src/app.py
@@ -5,22 +5,22 @@ from flask_marshmallow import Marshmallow
 app=Flask(__name__)
 CORS(app)
 # configuro la base de datos, con el nombre el usuario y la clave
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyectoclasePython'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
 #                                               user:clave@localhost/nombreBaseDatos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db= SQLAlchemy(app)
 ma=Marshmallow(app)
 # defino la tabla
-class Producto(db.Model):   # la clase Producto hereda de db.Model     
+class favoritos(db.Model):   # la clase Producto hereda de db.Model     
     id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
     nombre=db.Column(db.String(100))
-    precio=db.Column(db.Integer)
-    stock=db.Column(db.Integer)
+    valoracion=db.Column(db.Integer)
+    
     img=db.Column(db.String(100))
-    def __init__(self,nombre,precio,stock,img):   #crea el  constructor de la clase
+    def __init__(self,nombre,valoracion,img):   #crea el  constructor de la clase
         self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
-        self.precio=precio
-        self.stock=stock
+        self.valoracion=valoracion
+        
         self.img=img
 with app.app_context():
     db.create_all()  # crea las tablas
@@ -28,55 +28,52 @@ with app.app_context():
  
 class ProductoSchema(ma.Schema):
     class Meta:
-        fields=('id','nombre','precio','stock',"img")
+        fields=('id','nombre','valoracion','',"img")
 producto_schema=ProductoSchema()            # para crear un producto
 productos_schema=ProductoSchema(many=True)  # multiples registros
  
  
 # crea los endpoint o rutas (json)
-@app.route('/productos',methods=['GET'])
+@app.route('/favorito',methods=['GET'])
 def get_Productos():
-    all_productos=Producto.query.all()     # query.all() lo hereda de db.Model
+    all_productos=favoritos.query.all()     # query.all() lo hereda de db.Model
     result=productos_schema.dump(all_productos)  # .dump() lo hereda de ma.schema
     return jsonify(result)
  
  
-@app.route('/productos/<id>',methods=['GET'])
+@app.route('/favorito/<id>',methods=['GET'])
 def get_producto(id):
-    producto=Producto.query.get(id)
+    producto=favoritos.query.get(id)
     return producto_schema.jsonify(producto)
 
-@app.route('/productos/<id>',methods=['DELETE'])
+@app.route('/favorito/<id>',methods=['DELETE'])
 def delete_producto(id):
-    producto=Producto.query.get(id)
+    producto=favoritos.query.get(id)
     db.session.delete(producto)
     db.session.commit()
     return producto_schema.jsonify(producto)
 
-@app.route('/productos', methods=['POST']) # crea ruta o endpoint
+@app.route('/favorito', methods=['POST']) # crea ruta o endpoint
 def create_producto():
     print(request.json)  # request.json contiene el json que envio el cliente
     nombre=request.json['nombre']
-    precio=request.json['precio']
-    stock=request.json['stock']
+    valoracion=request.json['valoracion']
     img=request.json['img']
-    new_producto=Producto(nombre,precio,stock,img)
+    new_producto=favoritos(nombre,valoracion,img)
     db.session.add(new_producto)
     db.session.commit()
     return producto_schema.jsonify(new_producto)
 
-@app.route('/productos/<id>' ,methods=['PUT'])
+@app.route('/favorito/<id>' ,methods=['PUT'])
 def update_producto(id):
-    producto=Producto.query.get(id)
+    producto=favoritos.query.get(id)
    
     nombre=request.json['nombre']
-    precio=request.json['precio']
-    stock=request.json['stock']
+    valoracion=request.json['valoracion']
     img=request.json['img']
 
     producto.nombre=nombre
-    producto.precio=precio
-    producto.stock=stock
+    producto.valoracion=valoracion
     producto.img=img
     db.session.commit()
     return producto_schema.jsonify(producto)
