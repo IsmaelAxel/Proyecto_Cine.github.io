d429f3209edeec9b6264b864d70738c5737425b0
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2022-12-08 11:18:42 -0300

    favoritos

--- cine/src/app.py
+++ cine/src/app.py
@@ -34,26 +34,26 @@ productos_schema=ProductoSchema(many=True)  # multiples registros
  
  
 # crea los endpoint o rutas (json)
-@app.route('/favorito',methods=['GET'])
+@app.route('/favoritos',methods=['GET'])
 def get_Productos():
     all_productos=favoritos.query.all()     # query.all() lo hereda de db.Model
     result=productos_schema.dump(all_productos)  # .dump() lo hereda de ma.schema
     return jsonify(result)
  
  
-@app.route('/favorito/<id>',methods=['GET'])
+@app.route('/favoritos/<id>',methods=['GET'])
 def get_producto(id):
     producto=favoritos.query.get(id)
     return producto_schema.jsonify(producto)
 
-@app.route('/favorito/<id>',methods=['DELETE'])
+@app.route('/favoritos/<id>',methods=['DELETE'])
 def delete_producto(id):
     producto=favoritos.query.get(id)
     db.session.delete(producto)
     db.session.commit()
     return producto_schema.jsonify(producto)
 
-@app.route('/favorito', methods=['POST']) # crea ruta o endpoint
+@app.route('/favoritos', methods=['POST']) # crea ruta o endpoint
 def create_producto():
     print(request.json)  # request.json contiene el json que envio el cliente
     nombre=request.json['nombre']
@@ -64,7 +64,7 @@ def create_producto():
     db.session.commit()
     return producto_schema.jsonify(new_producto)
 
-@app.route('/favorito/<id>' ,methods=['PUT'])
+@app.route('/favoritos/<id>' ,methods=['PUT'])
 def update_producto(id):
     producto=favoritos.query.get(id)
    
