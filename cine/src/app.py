ecd8ed6183a49122f4de7c04f0b1ca98ac396930
IsmaelAxel <ismael_callamullo@hotmail.com>
2022-12-08 12:28:11 -0300

    app.py arreglado

--- cine/src/app.py
+++ cine/src/app.py
@@ -26,57 +26,57 @@ with app.app_context():
     db.create_all()  # crea las tablas
 #  ************************************************************
  
-class ProductoSchema(ma.Schema):
+class FavoritoSchema(ma.Schema):
     class Meta:
         fields=('id','nombre','valoracion','',"img")
-producto_schema=ProductoSchema()            # para crear un producto
-productos_schema=ProductoSchema(many=True)  # multiples registros
+favorito_schema=FavoritoSchema()            # para crear un producto
+favoritos_schema=FavoritoSchema(many=True)  # multiples registros
  
  
 # crea los endpoint o rutas (json)
 @app.route('/favoritos',methods=['GET'])
-def get_Productos():
-    all_productos=favoritos.query.all()     # query.all() lo hereda de db.Model
-    result=productos_schema.dump(all_productos)  # .dump() lo hereda de ma.schema
+def get_Favoritos():
+    all_favoritos=favoritos.query.all()     # query.all() lo hereda de db.Model
+    result=favoritos_schema.dump(all_favoritos)  # .dump() lo hereda de ma.schema
     return jsonify(result)
  
  
 @app.route('/favoritos/<id>',methods=['GET'])
-def get_producto(id):
-    producto=favoritos.query.get(id)
-    return producto_schema.jsonify(producto)
+def get_favorito(id):
+    favorito=favoritos.query.get(id)
+    return favorito_schema.jsonify(favorito)
 
 @app.route('/favoritos/<id>',methods=['DELETE'])
-def delete_producto(id):
-    producto=favoritos.query.get(id)
-    db.session.delete(producto)
+def delete_favorito(id):
+    favorito=favoritos.query.get(id)
+    db.session.delete(favorito)
     db.session.commit()
-    return producto_schema.jsonify(producto)
+    return favorito_schema.jsonify(favorito)
 
 @app.route('/favoritos', methods=['POST']) # crea ruta o endpoint
-def create_producto():
+def create_favorito():
     print(request.json)  # request.json contiene el json que envio el cliente
     nombre=request.json['nombre']
     valoracion=request.json['valoracion']
     img=request.json['img']
-    new_producto=favoritos(nombre,valoracion,img)
-    db.session.add(new_producto)
+    new_favorito=favoritos(nombre,valoracion,img)
+    db.session.add(new_favorito)
     db.session.commit()
-    return producto_schema.jsonify(new_producto)
+    return favorito_schema.jsonify(new_favorito)
 
 @app.route('/favoritos/<id>' ,methods=['PUT'])
-def update_producto(id):
-    producto=favoritos.query.get(id)
+def update_favorito(id):
+    favorito=favoritos.query.get(id)
    
     nombre=request.json['nombre']
     valoracion=request.json['valoracion']
     img=request.json['img']
 
-    producto.nombre=nombre
-    producto.valoracion=valoracion
-    producto.img=img
+    favorito.nombre=nombre
+    favorito.valoracion=valoracion
+    favorito.img=img
     db.session.commit()
-    return producto_schema.jsonify(producto)
+    return favorito_schema.jsonify(favorito)
 
  
  
